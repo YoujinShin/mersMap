bd0e2aad85e4cbeefb97cc9796cfb4a4d66ede24
Youjin Shin <meggonagul@gmail.com>
2015-07-01 23:01:20 -0400

    edit

---
+++ public/js/viz_scroll.js
@@ -0,0 +1,20 @@
+// var width = $('#viz').width(),
+// 	height = $('#viz').height();
+
+// var svg = d3.select('#viz').append('svg')
+// 	.attr('width', width)
+// 	.attr('height', height);
+
+// var tooltip = d3.select("body")
+//   .append("div")
+//   .attr("id", "tooltip");
+
+// var yScale = d3.time.scale()
+// 	.domain([ parseDate2('20-May'), parseDate2('21-Jun') ])
+// 	// .domain([ parseDate2('19-May'), parseDate2('17-Jun') ])
+// 	// .range([70, width - 70]);
+// 	.range([70, width - 70]);
+
+// var xScale = d3.scale.linear()
+// 	.domain([0, 39])
+// 	.range([height - 134, 100]);
